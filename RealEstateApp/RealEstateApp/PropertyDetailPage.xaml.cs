aefc7176f2fb1c0478f15ea7599090199eba5791
Jasmin Bidstrup Nielsen <jasm3956@elevcampus.dk>
2022-08-23 13:43:19 +0200

    Tested that Pitch and Volume works to the TTS.

--- RealEstateApp/RealEstateApp/PropertyDetailPage.xaml.cs
+++ RealEstateApp/RealEstateApp/PropertyDetailPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using RealEstateApp.Models;
 using RealEstateApp.Services;
 using System.Linq;
+using System.Threading;
 using TinyIoC;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -26,9 +28,58 @@ namespace RealEstateApp
 
         public Property Property { get; set; }
 
+        #region TEXT-TO-SPEECH PROPERTIES.
+
+        private CancellationTokenSource _cancellationToken;
+        private bool _isSpeaking;
+
+        public bool IsSpeaking
+        {
+            get => _isSpeaking;
+            set { _isSpeaking = value; OnPropertyChanged(nameof(IsSpeaking)); }
+        }
+
+        private float _selectedVolume = 1;
+
+        public float SelectedVolume
+        {
+            get => _selectedVolume;
+            set { _selectedVolume = value; OnPropertyChanged(nameof(SelectedVolume)); }
+        }
+
+        private float _selectedPitch = 1;
+
+        public float SelectedPitch
+        {
+            get => _selectedPitch;
+            set { _selectedPitch = value; OnPropertyChanged(nameof(SelectedPitch)); }
+        }
+
+        #endregion
+
         private async void EditProperty_Clicked(object sender, System.EventArgs e)
         {
             await Navigation.PushAsync(new AddEditPropertyPage(Property));
+        }
+
+        private async void SpeakButton_Clicked(object sender, System.EventArgs e)
+        {
+            _cancellationToken = new CancellationTokenSource();
+            IsSpeaking = true;
+            var speechOptions = new SpeechOptions
+            {
+                Volume = this.SelectedVolume,
+                Pitch = this.SelectedPitch
+            };
+            await TextToSpeech.SpeakAsync(Property.Description, speechOptions, _cancellationToken.Token);
+            IsSpeaking = false;
+        }
+
+        private void CancelSpeechButton_Clicked(object sender, System.EventArgs e)
+        {
+            if (_cancellationToken?.IsCancellationRequested ?? true)
+                return;
+            _cancellationToken.Cancel();
         }
     }
 }
